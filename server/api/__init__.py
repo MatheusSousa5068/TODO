dd5714cb88fcc9db4be26236d8135e8b64bf7ffe
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2023-06-26 11:06:39 -0300

    adding migrate and sqlalchemy

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -2,13 +2,21 @@ import os
 from dotenv import load_dotenv
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
 
 basedir = os.getcwd() # get path to current directory
 dotenv_path = os.path.join(basedir, '.env')
 load_dotenv(dotenv_path) # loads .env
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'server', 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+Migrate(app, db)
--- server/api/db/db.py
+++
@@ -1,6 +0,0 @@
-from api import app
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy(app)
-Migrate(app, db)
