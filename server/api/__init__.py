6d245d5855c81f07876affa33661b04ca24d641f
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2023-06-26 09:39:15 -0300

    adding configurations

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
-from flask_restful import Api
+import os
+from dotenv import load_dotenv
 
+from flask import Flask
 
 app = Flask(__name__)
 
+basedir = os.getcwd() # get path to current directory
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path) # loads .env
 
-@app.route('/')
-def index():
-    return '<p>Teste</p>'
+app.config['SECRET_KEY'] = os.environ.get('SECRET')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'server', 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
