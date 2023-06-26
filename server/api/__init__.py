38d12569fa92b6dc8c15b9e1e375df98a0c8d88a
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2023-06-26 14:46:59 -0300

    adding "is_done"

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -18,7 +18,7 @@ dotenv_path = os.path.join(basedir, '.env')
 load_dotenv(dotenv_path) # loads .env
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'server', 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -32,12 +32,13 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_text = db.Column(db.String(64))
     date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
+    is_done = db.Column(db.Boolean, default=False)
 
     def __init__(self, task_text):
         self.task_text = task_text
 
     def json(self):
-        return {'Task': self.task_text}
+        return {'Task': (self.task_text, self.is_done)}
     
 
     def __repr__(self):
@@ -57,10 +58,16 @@ class TasksRoutes(Resource):
 
     def put(self, text):
         task = Task.query.filter_by(task_text=text).first()
-        task.task_text = request.json['new_text']
-        db.session.commit()
 
+        if 'new_text' in request.json:
+            task.task_text = request.json['new_text']
+            
+        if 'status' in request.json:
+            task.is_done = not task.is_done
+
+        db.session.commit()
         return task.json()
+    
 
     
 class AllTasks(Resource):
